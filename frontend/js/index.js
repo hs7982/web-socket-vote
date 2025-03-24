5395b94809e188100fdeb8425f185178145dd0de
hseok <17452867+hs7982@users.noreply.github.com>
2025-03-24 17:19:30 +0900

    feat: [#3] 홈 페이지 추가

---
+++ frontend/js/index.js
@@ -0,0 +1,32 @@
+//백엔드 완료 전까지 임시로 사용할 데이터
+const postData = [{ "id": "1", "title": "투표제목", "subtitle": "투표설명" },
+{ "id": "2", "title": "투표제목", "subtitle": "투표설명" },
+{ "id": "3", "title": "투표제목", "subtitle": "투표설명" },
+{ "id": "4", "title": "투표제목", "subtitle": "투표설명" },
+{ "id": "5", "title": "투표제목", "subtitle": "투표설명" },
+{ "id": "6", "title": "투표제목", "subtitle": "투표설명" },
+{ "id": "7", "title": "투표제목", "subtitle": "투표설명" },
+{ "id": "8", "title": "투표제목", "subtitle": "투표설명" },];
+
+try {
+    //TODO: 투표방 목록 가져오는 api 연결
+
+} catch (error) {
+    console.error(error)
+}
+
+console.log(postData)
+
+const listBox = document.getElementsByClassName("listBox")[0]
+for (const post of postData) {
+    const postHTML = `
+        <div class="listItem">
+            <h3>${post.title}</h3>
+            <p>${post.subtitle}</p>
+            <a href="./vote.html?id=${post.id}" class="btn btn_pr btn_sm" onclick="gotoVote(${post.id})">투표하기</a>
+        </div>
+    `;
+
+    listBox.insertAdjacentHTML('beforeend', postHTML);
+    
+}
