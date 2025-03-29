8cc68f439f45262db2606780d8e37376a12cd7db
hseok <17452867+hs7982@users.noreply.github.com>
2025-03-30 03:04:14 +0900

    feat: [#8] 유저 권한 기능 추가

--- vote/src/main/java/com/hseok/vote/controller/UserController.java
+++ vote/src/main/java/com/hseok/vote/controller/UserController.java
@@ -1,14 +1,13 @@
-package com.hseok.vote.user;
+package com.hseok.vote.controller;
 
+import com.hseok.vote.user.UserService;
 import com.hseok.vote.user.dto.UserJoinRequest;
 import com.hseok.vote.user.dto.UserJoinResponse;
+import com.hseok.vote.util.ResponseHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -17,8 +16,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<UserJoinResponse> join(@RequestBody UserJoinRequest reqDto) {
+    public ResponseEntity<Object> join(@RequestBody UserJoinRequest reqDto) {
         UserJoinResponse result = userService.userJoin(reqDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        return ResponseHandler.responseBuilder(
+                HttpStatus.OK,
+                "계정이 생성되었습니다.",
+                result
+        );
     }
 }
--- vote/src/main/java/com/hseok/vote/user/UserService.java
+++ vote/src/main/java/com/hseok/vote/user/UserService.java
@@ -1,5 +1,7 @@
 package com.hseok.vote.user;
 
+import com.hseok.vote.repository.RoleRepository;
+import com.hseok.vote.user.domain.Role;
 import com.hseok.vote.user.domain.User;
 import com.hseok.vote.user.dto.UserJoinRequest;
 import com.hseok.vote.user.dto.UserJoinResponse;
@@ -9,11 +11,16 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.RoleNotFoundException;
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     public User getUserById(Long id) {
         return userRepository.findById(id).orElseThrow(()->new UsernameNotFoundException("User not found"));
@@ -27,12 +34,18 @@ public class UserService {
         validateDuplicateUsername(reqDto.getUsername());
         validateDuplicateEmail(reqDto.getEmail());
 
+        Role userRole = roleRepository.findByName("ROLE_USER")
+                .orElseThrow(() -> new RuntimeException("기본 역할을 찾을 수 없습니다."));
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(userRole);
+
         User user = User.builder()
                 .username(reqDto.getUsername())
                 .email(reqDto.getEmail())
                 .password(passwordEncoder.encode(reqDto.getPassword()))
                 .enabled(true)
-
+                .roles(roles)
                 .build();
 
         userRepository.save(user);
