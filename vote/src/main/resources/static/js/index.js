e71d593780c225e455779628c7d050c71ddac644
hseok <17452867+hs7982@users.noreply.github.com>
2025-03-31 00:27:41 +0900

    feat: [#3] 투표목록 api 연결

--- vote/src/main/resources/static/js/index.js
+++ vote/src/main/resources/static/js/index.js
@@ -1,36 +1,35 @@
-import { api_test } from "../js/api.js";
+import {api_getVoteList, api_test} from "../js/api.js";
 
 //백엔드 완료 전까지 임시로 사용할 데이터
-const postData = [{ "id": "1", "title": "투표제목", "subtitle": "투표설명" },
-{ "id": "2", "title": "투표제목", "subtitle": "투표설명" },
-{ "id": "3", "title": "투표제목", "subtitle": "투표설명" },
-{ "id": "4", "title": "투표제목", "subtitle": "투표설명" },
-{ "id": "5", "title": "투표제목", "subtitle": "투표설명" },
-{ "id": "6", "title": "투표제목", "subtitle": "투표설명" },
-{ "id": "7", "title": "투표제목", "subtitle": "투표설명" },
-{ "id": "8", "title": "투표제목", "subtitle": "투표설명" },];
+// const postData = [{ "id": "1", "title": "투표제목", "subtitle": "투표설명" },
+// { "id": "2", "title": "투표제목", "subtitle": "투표설명" },
+// { "id": "3", "title": "투표제목", "subtitle": "투표설명" },
+// { "id": "4", "title": "투표제목", "subtitle": "투표설명" },
+// { "id": "5", "title": "투표제목", "subtitle": "투표설명" },
+// { "id": "6", "title": "투표제목", "subtitle": "투표설명" },
+// { "id": "7", "title": "투표제목", "subtitle": "투표설명" },
+// { "id": "8", "title": "투표제목", "subtitle": "투표설명" },];
 
-try {
-    //TODO: 투표방 목록 가져오는 api 연결
-
-} catch (error) {
-    console.error(error)
+async function getListData() {
+    const result = await api_getVoteList();
+    const voteData = await result.json();
+    return voteData.data;
 }
 
+const postData =  await getListData();
 console.log(postData)
-
 const listBox = document.getElementsByClassName("listBox")[0]
 for (const post of postData) {
     const postHTML = `
         <div class="listItem">
             <h3>${post.title}</h3>
-            <p>${post.subtitle}</p>
+            <p>${post.content}</p>
             <a href="./vote?id=${post.id}" class="btn btn_pr btn_sm" onclick="gotoVote(${post.id})">투표하기</a>
+            <span>${post.voteCount}명 참여중</span>
         </div>
     `;
 
     listBox.insertAdjacentHTML('beforeend', postHTML);
-    
+
 }
 
-api_test();
